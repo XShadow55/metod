9341388b75af962b84f409044ae7b38ec68cc7ab
Свинухов константин <lopata216@yandex.ru>
2023-09-01 07:37:29 +0300

    методы

--- src/Main.java
+++ src/Main.java
@@ -9,72 +9,72 @@ public class Main {
         zd2();
         zd3();
     }
-    public static void zd1(){
+
+    public static void zd1() {
         int year = 2000;
         leapYear(year);
 
 
+    }
+
+    public static void leapYear(int year) {
+
+        if (year % 4 == 0 && year%100!=0) {
+            System.out.println(year + " год — високосный год ");
+
+        } else if (year % 400 == 0) {
+            System.out.println(year + " год — високосный год ");
+        } else {
+            System.out.println(year + " год — невисокосный год ");
+        }
 
     }
-    public static void leapYear(int year){
 
-        if (year%10 == 4 | year%10 == 8 | year%10 == 6 | year%10 == 2){
-            System.out.println(year+" год — високосный год ");
-
-        }
-        else if ( year%400 == 0){
-            System.out.println(year+" год — високосный год ");
-        }
-            else {
-                System.out.println(year+" год — невисокосный год ");
-            }
-
-    }
-    public static void zd2(){
+    public static void zd2() {
         int type = 1;
         int year = 1223;
-        String clientOC = typeOC(type);
         String dataPhone = version(year);
-        System.out.println("Установите "+ dataPhone+" версию приложения для "+clientOC+" по ссылке");
+        System.out.println("Установите " + version(year) + " версию приложения для " + typeOC(type) + " по ссылке");
 
     }
-    public static String typeOC(int type){
-        String name = "";
-        if (type == 0){
-            name = "iOS";
 
-        }
-        else{
-            name = "Android";
-        }
-        return name;
-    }
-    public static String version(int ver){
-        String name = "";
-        if (ver == LocalDate.now().getYear()){
-            name = "обычную";
+    public static String typeOC(int type) {
 
+        if (type == 0) {
+            return "iOS";
+
+        } else {
+            return "Android";
         }
-        else {
-            name = "облегченную";
-        }
-        return name;
+
     }
-    public static void zd3(){
+
+    public static String version(int ver) {
+
+        if (ver == LocalDate.now().getYear()) {
+            return "обычную";
+
+        } else {
+            return "облегченную";
+        }
+
+    }
+
+    public static void zd3() {
         int deliveryDistance = 335;
-        System.out.println("Потребуется дней: "+days(deliveryDistance));
+        System.out.println("Потребуется дней: " + days(deliveryDistance));
     }
-    public static String days(int km){
-        if (km < 20){
+
+    public static String days(int km) {
+        if (km < 20) {
             return "1 ";
         }
-        if (km >=20 && km < 60){
+        if (km >= 20 && km < 60) {
             return "2 ";
         }
-        if (km >=60 && km < 100){
+        if (km >= 60 && km < 100) {
             return "3 ";
-        }
-        else {
+        } else {
             return "доставки нет";
         }
     }
